d0d4c95dc676e2bf1a7e6817fb0b675bf78dd17f
benjamin.pelletier <pelletierbenjamin90@gmail.com>
2020-03-08 11:28:05 +0100

    Update Services : - Add OrderService - Remove UserOrderService

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -8,19 +8,54 @@ use App\Entity\UserOrderDetail;
 use App\Entity\UserOrderStatus;
 use App\Repository\ProductRepository;
 use App\Repository\StatusOrderRepository;
+use App\Repository\UserOrderDetailsRepository;
+use App\Repository\UserOrderRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
-class UserOrderService {
+class OrderService {
 
     private $userOrder;
     private $manager;
     private $statusOrderRepository;
     private $productRepository;
 
-    public function __construct(EntityManagerInterface $manager, StatusOrderRepository $statusOrderRepository, ProductRepository $productRepository){
+    public function __construct(EntityManagerInterface $manager,
+                                StatusOrderRepository $statusOrderRepository,
+                                ProductRepository $productRepository,
+                                UserOrderRepository $userOrderRepository, UserOrderDetailsRepository $userOrderDetailsRepository){
         $this->manager = $manager;
         $this->statusOrderRepository = $statusOrderRepository;
         $this->productRepository = $productRepository;
+        $this->userOrderRepository = $userOrderRepository;
+        $this->userOrderDetailsRepository = $userOrderDetailsRepository;
+    }
+
+    public function orderExist($paiementId) :bool {
+        $order = $this->userOrderRepository->findOneBy(['paiementId' => $paiementId]);
+        return $order ==! null;
+    }
+
+    public function getOrder($paiementId) :UserOrder {
+        return $this->userOrderRepository->findOneBy(['paiementId' => $paiementId]);
+    }
+
+    public function getProductsOrder($order) {
+        return $this->userOrderDetailsRepository->findBy(['userOrder' => $order]);
+    }
+
+    public function getTotalEur($paiementId) :float {
+        $productsOrder = $this->getProductsOrder($this->getOrder($paiementId));
+        $total = 0;
+        foreach ($productsOrder as $productOrder) {
+            $totalRow = $productOrder->getPrice()*$productOrder->getQuantityOrder();
+            $total = $total+$totalRow;
+        }
+        return $total;
+    }
+
+    //temp for test
+    public function getTotalXMR($paiementId) :float {
+        return 1.22;
     }
 
     public function createUserOrder() :void {
@@ -60,5 +95,10 @@ class UserOrderService {
 
     public function getUserOrder() {
         return $this->userOrder;
+    }
+
+    public function orderIsLocked(String $paiementId) {
+        $order = $this->userOrderRepository->findOneBy(['paiementId' => $paiementId]);
+        return $order->getIsLock();
     }
 }
